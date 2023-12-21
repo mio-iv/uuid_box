802e52a1c909f292be74e05ff43607decebc2c3a
飯田菜摘 <natsumi@iidanatsuminoMacBook-Air.local>
2023-12-21 16:03:34 +0900

    異常ファイル追加

---
+++ main.py
@@ -0,0 +1,9 @@
+i = 10
+for i in range(10):
+    print(i)
+
+
+test = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
+
+import os
